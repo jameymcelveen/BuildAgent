79e5532ca8c3d4675f682259d78a393a3f5024e8
Jamey McElveen <jamey@mcelveen.us>
2022-11-06 04:04:30 -0500

    First Tag

--- Program.cs
+++ Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using BuildAgent;
 
+var dest = "/users/jameymcelveen/dev/BuildAgent2";
+var work = "/users/jameymcelveen/dev/work";
+
+if (Directory.Exists(dest))
+{
+    Directory.Delete(dest, true);
+}
+if (Directory.Exists(work))
+{
+    Directory.Delete(work, true);
+}
+var proc = new Process();
+proc.StartInfo.FileName = @"git";
+proc.StartInfo.Arguments = "clone git@github.com:jameymcelveen/BuildAgent.git " + work;
+proc.StartInfo.UseShellExecute = false;
+proc.StartInfo.RedirectStandardOutput = true;
+proc.Start();
+string outPut = proc.StandardOutput.ReadToEnd();
+Console.WriteLine(outPut);
+proc.WaitForExit();
+var exitCode = proc.ExitCode;
+proc.Close();
+
+Directory.SetCurrentDirectory(work);
+Directory.CreateDirectory(dest);
+
+proc = new Process();
+proc.StartInfo.FileName = @"dotnet";
+proc.StartInfo.Arguments = "build --configuration Release --output " + dest;
+proc.StartInfo.UseShellExecute = false;
+proc.StartInfo.RedirectStandardOutput = true;
+proc.Start();
+outPut = proc.StandardOutput.ReadToEnd();
+Console.WriteLine(outPut);
+proc.WaitForExit();
+exitCode = proc.ExitCode;
+proc.Close();
---
+++ Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BuildAgent
+{
+	public static class Utils
+	{
+        public static void ExecuteCommand(string Command)
+        {
+            ProcessStartInfo ProcessInfo;
+            Process Process;
+
+            ProcessInfo = new ProcessStartInfo("cmd.exe", "/K " + Command);
+            ProcessInfo.CreateNoWindow = true;
+            ProcessInfo.UseShellExecute = true;
+
+            Process = Process.Start(ProcessInfo);
+        }
+    }
+}
+
